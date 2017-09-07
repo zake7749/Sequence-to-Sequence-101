1e7104044444281953dccaa8ed65ebc30df0802e
zake7749 <zake7749@gmail.com>
2017-09-07 13:21:17 +0800

    Add a default config file.

---
+++ Epoch1-BasicSeq2Seq/config/config.py
@@ -0,0 +1,16 @@
+import torch
+# Define hyper parameter
+use_cuda = True if torch.cuda.is_available() else False
+
+# for dataset
+dataset_path = 'dataset/Google-10000-English.txt'
+
+# for training
+num_epochs = 50
+batch_size = 128
+learning_rate = 1e-3
+
+# for model
+encoder_embedding_size = 128
+encoder_output_size = 256
+decoder_hidden_size = encoder_output_size
