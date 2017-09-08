e80656c69b932d11f9f8c34bf120f020fb218aa0
zake7749 <zake7749@gmail.com>
2017-09-08 12:31:42 +0800

    Add an interface for evaluating.

---
+++ Epoch1-BasicSeq2Seq/eval.py
@@ -0,0 +1,39 @@
+from model.Encoder import VanillaEncoder
+from model.Decoder import VanillaDecoder
+from model.Seq2Seq import Seq2Seq
+from dataset.DataHelper import DataTransformer
+from train import Trainer
+from config import config
+
+
+def main():
+    data_transformer = DataTransformer(config.dataset_path, use_cuda=config.use_cuda)
+
+    # define our models
+    vanilla_encoder = VanillaEncoder(vocab_size=data_transformer.vocab_size,
+                                     embedding_size=config.encoder_embedding_size,
+                                     output_size=config.encoder_output_size)
+    vanilla_decoder = VanillaDecoder(hidden_size=config.decoder_hidden_size,
+                                     output_size=data_transformer.vocab_size)
+
+    if config.use_cuda:
+        vanilla_encoder = vanilla_encoder.cuda()
+        vanilla_decoder = vanilla_decoder.cuda()
+
+    seq2seq = Seq2Seq(encoder=vanilla_encoder,
+                      decoder=vanilla_decoder,
+                      sos_index=data_transformer.SOS_ID,
+                      use_cuda=config.use_cuda)
+
+    trainer = Trainer(seq2seq, data_transformer, config.learning_rate, config.use_cuda)
+    trainer.load_model()
+
+    while(True):
+        testing_word = input('You say: ')
+        if testing_word == "exit":
+            break
+        results = trainer.evaluate(testing_word)
+        print("Model says: %s" % results[0])
+
+if __name__ == "__main__":
+    main()
