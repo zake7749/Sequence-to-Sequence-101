7c941f3ed3c2cf4c4fa447734bb21b92a57f9708
ss12f32v <cloudstrife60138@gmail.com>
2017-09-09 18:15:42 +0800

    Completed.

--- Epoch1-BasicSeq2Seq/tensorflow_ver/tf_model.py
+++ Epoch1-BasicSeq2Seq/tensorflow_ver/tf_model.py
@@ -11,8 +11,9 @@ print("Make sure your tf version is the latest 1.3.0 , Your tf version: ",tf.__v
 
 class Model():
 	def __init__(self, encoder_embedding_size, encoder_output_size, batch_size, vocab_size, forward_only= None):
-		#define the model hyperarameters here
+		# Define the model hyperarameters here
 		"""
+			max_time means the input  length of this batch
 			encoder_inputs int32 tensor is shaped [encoder_max_time, batch_size]
 			decoder_targets int32 tensor is shaped [decoder_max_time, batch_size]
 			decoder_inputs int32 tensor is shaped [decoder_max_time, batch_size]
@@ -22,20 +23,22 @@ class Model():
 		self.decoder_targets = tf.placeholder(shape=(None, self.batch_size), dtype=tf.int32, name='decoder_targets')
 		self.decoder_inputs = tf.placeholder(shape=(None, self.batch_size), dtype=tf.int32, name='decoder_inputs')
 
-		self.encoder_inputs_inf = tf.placeholder(shape=(None, 1), dtype=tf.int32, name='encoder_inputs_inf')
-		self.input_sequence_length_inf =tf.placeholder(shape=([1]), dtype=tf.int32, name='input_length_inf')
-		self.decoder_inputs_inf = tf.placeholder(shape=(None, 1), dtype=tf.int32, name='decoder_inputs')
-
 		self.input_sequence_length = tf.placeholder(shape=([self.batch_size]), dtype=tf.int32, name='input_length')
 		self.decoder_sequence_length = tf.placeholder(shape=([self.batch_size]), dtype=tf.int32, name='decoder_inputs_length')
 		self.target_sequence_length = tf.placeholder(shape=([self.batch_size]), dtype=tf.float32, name='target_sequence_length')
 
-		self.forward_only = forward_only
 		self.encoder_hidden_size = encoder_embedding_size
 		self.decoder_hidden_size = encoder_output_size 
+		
+		"""Inference placeholder(Not yet finished the custom test part )"""
+		# self.encoder_inputs_inf = tf.placeholder(shape=(None, 1), dtype=tf.int32, name='encoder_inputs_inf')
+		# self.input_sequence_length_inf =tf.placeholder(shape=([1]), dtype=tf.int32, name='input_length_inf')
+		# self.decoder_inputs_inf = tf.placeholder(shape=(None, 1), dtype=tf.int32, name='decoder_inputs')
+		# self.forward_only = forward_only
+		
 
-		print("d hidden size",self.decoder_hidden_size)
-		#Embedding:
+		
+		# Embedding
 		"""	
 			We can use the one hot vector in the Datahelper to find the index of the vector in the emdbeddings
 		"""
@@ -43,11 +46,11 @@ class Model():
 		embeddings = tf.get_variable(name='embedding',
 			shape=[vocab_size, encoder_embedding_size],
 			initializer=initializer, dtype=tf.float32)
-
-		# embeddings = tf.get_variable(tf.random_uniform([vocab_size, encoder_embedding_size], -1.0, 1.0), dtype=tf.float32)
-		encoder_cell = GRUCell(self.encoder_hidden_size)
 		encoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, self.encoder_inputs)
 		decoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, self.decoder_inputs)
+
+		# Define the Cell used in the RNN. We use GRU in this model(Encoder & Decoder)
+		encoder_cell = GRUCell(self.encoder_hidden_size)
 		encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(
 											encoder_cell, 
 											encoder_inputs_embedded,
@@ -56,17 +59,17 @@ class Model():
 											time_major=True
 											)
 		
-		
-	
-		
-		#Decoder
+		# Decoder cell
 		decoder_cell = GRUCell(self.decoder_hidden_size)
-		#helper
+		
+		# Training Helper
 		helper = tf.contrib.seq2seq.TrainingHelper(
                 decoder_inputs_embedded, self.decoder_sequence_length, time_major=True)
-		#project layer
+		
+		# Project layer (Project the output to this layer(Softmax output). It's shape is [1*vocab_size].)
 		output_layer = layers_core.Dense(vocab_size , use_bias=False, name="output_projection")
 
+		# Define the decoder instance and feed it in to dynamic_decode function
 		decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell,
 												helper,
 												encoder_final_state,
--- Epoch1-BasicSeq2Seq/tensorflow_ver/train.py
+++ Epoch1-BasicSeq2Seq/tensorflow_ver/train.py
@@ -4,13 +4,14 @@ import tensorflow as tf
 import numpy as np 
 
 
-
-#hype parameters for training
+ 
+# Hype parameters for training
 epochs = 50
 batch_size = 100
 learning_rate = 1e-3
 grad_clip = 0.3
-# model parameter 
+
+# Model parameter 
 encoder_embedding_size = 128
 decoder_hidden_size = encoder_output_size = 128
 
@@ -18,9 +19,8 @@ decoder_hidden_size = encoder_output_size = 128
 
 class trainer_class():
 	def train(self, model, data_transformer):
-
+		#Define some parameter and the optimizer here 
 		n_iter = 0
-		loss = 0 
 		tvars = tf.trainable_variables()
 		grads, _ = tf.clip_by_global_norm(tf.gradients(model.train_loss, tvars), grad_clip)
 		optimizer = tf.train.AdamOptimizer(learning_rate)
@@ -30,23 +30,15 @@ class trainer_class():
 			sess.run(tf.global_variables_initializer())
 			for epoch in range(0, epochs):
 				input_batches, target_batches = data_transformer.mini_batches(batch_size=batch_size)
-				for input_batch, target_batch in zip(input_batches, target_batches):
+				for input_batch, target_batch in zip(input_batches, target_batches):	
 					input_length = np.array(input_batch[1])
 					target_length = np.array(target_batch[1])
 					input_batch = np.transpose(np.array(input_batch[0]))
 					target_batch = np.transpose(np.array(target_batch[0]))
 					n_iter +=1
-					# print ("input batch:",input_batch)
-					# print ("target batch", target_batch)
-					# print ("input length",input_length)
-					# print ("target length", target_length)
-
+				
 					if target_length.shape[0] == batch_size:
-						
-					# print("B0-0, Inputs")
-					# print(input_batch[0],"\n", input_batch[1])
-					# print("Targets")
-					# print(target_batch[0],"\n",target_batch[1])
+						#Feed the batch into the training model 
 						output, loss, _ = sess.run([model.decoder_logits,model.train_loss,train_op],feed_dict={
 								model.encoder_inputs: input_batch,
 								model.decoder_targets: target_batch,
@@ -55,12 +47,11 @@ class trainer_class():
 								model.decoder_sequence_length: target_length,
 								model.target_sequence_length: target_length
 							})
-
+						#Print loss and result after training 50 epoch
 						if n_iter % 50 == 0:
-							print ("loss:",loss)
-							
 							Predict_Words =[]
 							Input_words = []
+							# Two for loops are used to discard the "EOS"
 							for batch in output:
 								word = []
 								for prob in batch:
@@ -74,20 +65,21 @@ class trainer_class():
 							for batch in np.transpose(input_batch):
 								word= data_transformer.vocab.indices_to_sequence(batch)
 								Input_words.append(word)
+							
+							print("-----{}epochs------------- ".format(n_iter))
+							print("loss: ",loss,"\n")
+							print("Input: ",Input_words,"\n")
+							print("Predict: ",Predict_Words,"")
+							print("--------------------------\n ")
 
-							print("Input",Input_words)
-							print("Predict",Predict_Words,"\n")
 						
-	def evaluate(self, words, data_transformer):	
-		
-		results = data_transformer.vocab.sequence_to_indices(words)
-		length = len(results)
-		
-		return results, length
 
 def main():
 	data_transformer = DataTransformer('../dataset/Google-10000-English.txt', use_cuda=False)
-	seq2seq_model = Model(encoder_embedding_size, encoder_output_size,batch_size,vocab_size= data_transformer.vocab_size)
+	seq2seq_model = Model(  encoder_embedding_size = encoder_embedding_size, 
+							encoder_output_size = encoder_output_size,
+							batch_size = batch_size,
+							vocab_size= data_transformer.vocab_size)
 	trainer = trainer_class()
 	trainer.train(seq2seq_model, data_transformer)
 
